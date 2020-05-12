7812549356e9f38d294c2fca84902704d5c1481b
root <root@kris-modern.localdomain>
2020-05-11 19:31:49 -0700

    trial1

---
+++ precache-manifest.197bb88bf328bb5c36fc3cf03f1c298a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2800466c20c26feaac4e8af33f88ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d39d1b0360d2eb57324",
+    "url": "/static/css/main.d20d55f1.chunk.css"
+  },
+  {
+    "revision": "c08dec3b04a97cb2f15f",
+    "url": "/static/js/2.13489c2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13489c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d39d1b0360d2eb57324",
+    "url": "/static/js/main.8d80d3e7.chunk.js"
+  },
+  {
+    "revision": "60a96fed028770ab42a5",
+    "url": "/static/js/runtime-main.32e4a320.js"
+  }
+]);
